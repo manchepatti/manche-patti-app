f8b6e68fa62fdf44595ce2760023ef63f7ab0df8
manchepatti <jamatulmuslimeenmanche@gmail.com>
2025-07-17 16:24:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,5 @@ def generate():
     return send_file(file_name, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
