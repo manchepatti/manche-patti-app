cf1038863836d8d31a083a9ffdd14ef8c4f9f6d5
manchepatti <jamatulmuslimeenmanche@gmail.com>
2025-07-18 10:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-# app.py
-
+# === app.py ===
 from flask import Flask, render_template, request, send_file
-from weasyprint import HTML, CSS
+import pdfkit
 import uuid
 import os
 from num2words import num2words
 from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
@@ -18,11 +19,15 @@ def generate():
     data = request.form.to_dict()
     total = 0
     i = 1
+    descriptions = []
+
     while f'desc{i}' in data or f'charge{i}' in data:
         try:
-            total += float(data.get(f'charge{i}', 0))
+            charge = float(data.get(f'charge{i}', 0))
+            total += charge
         except ValueError:
-            pass
+            charge = 0
+        descriptions.append(f"{data.get(f'desc{i}', '')} - {charge}")
         i += 1
 
     data['item_count'] = i - 1
@@ -39,25 +44,49 @@ def generate():
 
     data['date'] = formatted_date_display
 
-    # Font paths
+    # Save to Google Sheet
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+    client = gspread.authorize(creds)
+
+    sheet_name = os.environ.get("GOOGLE_SHEET_NAME", "Patti_Form_Data")
+    sheet = client.open(sheet_name).sheet1
+
+    desc_combined = "\n".join(descriptions)
+    sheet.append_row([
+        formatted_date_display,
+        data.get('bill_no', ''),
+        data.get('name', ''),
+        data.get('address', ''),
+        desc_combined,
+        data.get('total', ''),
+        data.get('total_words', ''),
+        data.get('comments', '')
+    ])
+
     urdu_font_path = os.path.abspath("static/fonts/NotoNastaliqUrdu-Regular.ttf")
     marathi_font_path = os.path.abspath("static/fonts/NotoSansDevanagari-Regular.ttf")
 
-    rendered_html = render_template('receipt_template.html', data=data,
-                                    urdu_font_path=urdu_font_path, marathi_font_path=marathi_font_path)
+    rendered = render_template('receipt_template.html', data=data,
+                               urdu_font_path=urdu_font_path, marathi_font_path=marathi_font_path)
 
-    file_name = f"{data['bill_no'].replace(' ', '_')}_{data['name'].replace(' ', '_')}_{formatted_date_filename}.pdf"
+    safe_bill = data['bill_no'].replace(" ", "_")
+    safe_name = data['name'].replace(" ", "_")
+    file_name = f"{safe_bill}_{safe_name}_{formatted_date_filename}.pdf"
 
-    html = HTML(string=rendered_html)
-    css = CSS(string='''
-        @page { size: A5; margin: 10mm; }
-        body { font-family: 'DejaVu Sans'; }
-    ''')
-    html.write_pdf(file_name, stylesheets=[css])
+    # Use default wkhtmltopdf
+    config = pdfkit.configuration()
+    options = {
+        'page-size': 'A5',
+        'encoding': 'UTF-8',
+        'margin-top': '10mm',
+        'margin-bottom': '10mm',
+        'margin-left': '10mm',
+        'margin-right': '10mm'
+    }
 
+    pdfkit.from_string(rendered, file_name, configuration=config, options=options)
     return send_file(file_name, as_attachment=True)
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
